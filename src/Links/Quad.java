1eb38f6b63aa6c4d62da769e446f675b3912c91d
Extranjero13 <arivera0229@yahoo.com>
2023-04-25 19:16:59 -0400

    Update 2

--- src/Links/Quad.java
+++ src/Links/Quad.java
@@ -4,21 +4,24 @@ import java.lang.Math;
 public class Quad {
     public double Compute_dis(int a , int b , int c){
         double j;           //discriminant 
-        double k;
         
-        k= Math.pow(b, 2) - 4*(a)*(c);
-        j=Math.sqrt(k);
+        
+        j= Math.pow(b, 2) - 4*(a)*(c);
         return j;
     }
 
-    public double Formula(double j , int b , int a){
+    public double Formula(double j , int b , int a, int c){
         double x;
-        x= (-b + j)/2*a;
+        double k= Math.pow(b, 2) - 4*(a)*(c);
+        double u = Math.sqrt(k);
+                x= (-b + u)/2*a;
         return x;
     }
-    public double Formula_Neg(double j , int b , int a){
+    public double Formula_Neg(double j , int b , int a, int c){
         double x;
-        x= (-b - j)/2*a;
+        double k = Math.pow(b,2) - 4*(a)*(c);
+        double u = Math.sqrt(k);
+            x= (-b - u)/2*a;
         return x;
     }
 }
--- src/Links/SolveEq.java
+++ src/Links/SolveEq.java
@@ -1,34 +1,27 @@
 package Links;
-import java.util.Scanner;
 public class SolveEq {
-            // we do not need user input, get rid of this later.
+            
     public static void main(String[] args) throws Exception {
-        Scanner calculate = new Scanner(System.in);
-        Holder trialone = new Holder();
+
+        //Holder trialone = new Holder();
         Quad ExampleOne = new Quad();
-        System.out.println("Welcome to your Quadratic Formula character. Please enter integer values for a,b, and c");
-        System.out.print("input value for a:");
-        int a = calculate.nextInt();
-        trialone.setA(a);
-        System.out.print("input value for b:");
-        int b = calculate.nextInt();
-        trialone.setB(b);
-        System.out.print("input value for c:");
-        int c = calculate.nextInt();
-        trialone.setC(c);
+        Quad ExampleTwo = new Quad();
+        Comp tarea = new Comp(-1.5,1.32288);
 
-        calculate.close();
+        System.out.println("This discriminant is greater than 0. The coefficients are a =1 , b=65 , c=85");
+        System.out.println("Discriminant equals = " +  ExampleOne.Compute_dis(1, 65 , 85)); // discriminant test case with two real roots
+        double j= ExampleOne.Compute_dis(1, 65, 85);
+        System.out.println("Your two real roots are as follows: ");
+        System.out.println("x= " + ExampleOne.Formula(j, 65, 1,85)); // Quad Formula Solved for Positive Value
+        System.out.println("x= " + ExampleOne.Formula_Neg(j, 65, 1,85)); // Quad Formula Solved for Negative Value
 
+        System.out.println("------------------------------------------------");
+        System.out.println("The discriminant is less than 0. The coeffecients are a=1 , b=3 , c=4"); 
+        // we are testing two complex/imaginary roots
 
-        System.out.println(trialone.getA());
-        System.out.println(trialone.getB());
-        System.out.println(trialone.getC());
-
-        System.out.println(ExampleOne.Compute_dis(a, b , c)); // discriminant
-        double j= ExampleOne.Compute_dis(a, b, c);
-        System.out.println(ExampleOne.Formula(j, b, a)); // Quad Formula Solved for Positive Value
-        System.out.println(ExampleOne.Formula_Neg(j, b, a)); // Quad Formula Solved for Negative Value
-
+       System.out.println("Discriminant equals = " +  ExampleTwo.Compute_dis(1, 3 , 4));
+            tarea.new_Format();
+            tarea.neg_Format();
 
     }
 }
